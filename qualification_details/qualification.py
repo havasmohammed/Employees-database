3e2c53e50c0dde647a94f55524e89a6c2db5a3c1
havasmohammed <havas@qburst.com>
2015-07-16 17:26:37 +0530

    modified by adding try catch, choices for designationid, followed coding styles

--- qualification_details/qualification.py
+++ qualification_details/qualification.py
@@ -1,13 +1,20 @@
 import MySQLdb   
 db = MySQLdb.connect(host="localhost", user="root",passwd="havas1993z",db="Employees")
-x='y'
-while x=='y':
-	designationid=int(raw_input("Enter designationid choices 1.441 2.442 3.443 4.444 5.445 6.446 "))
-	qualification=raw_input( "Enter qualification: ")
-	branch=raw_input( "Enter branch: ")
-	#db= pyodbc.connect('DRIVER={SQL Server};SERVER=localhoast;DATABASE=tutorial')
+X = 'y'
+try:
 	cursor = db.cursor()
-	cursor.execute("Insert into qualification_details Values (%s,%s,%s)",(designationid, qualification, branch))
-	x=raw_input("do u want to continue(y/n)")
-db.commit()
+	while X == 'y':
+		print "enter the designationid from the list"
+		cursor.execute("select * from designation_details")
+		for row in cursor.fetchall():
+			print "designationid: %d,designation: %s"%(row[0],row[1])
+		designationid=int(raw_input())
+		qualification = raw_input( "Enter qualification: ")
+		branch = raw_input( "Enter branch: ")
+		#db= pyodbc.connect('DRIVER={SQL Server};SERVER=localhoast;DATABASE=tutorial')
+		cursor.execute("Insert into qualification_details Values (%s,%s,%s)",(designationid, qualification, branch))
+		X = raw_input("do u want to continue(y/n)")
+except Exception as e:
+	print str(e)
+	db.commit()
 cursor.close()
