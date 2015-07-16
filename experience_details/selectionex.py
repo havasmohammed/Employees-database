5c679e209924c40ddc3fe59fb38fdade8d953eb6
havasmohammed <havas@qburst.com>
2015-07-16 17:23:26 +0530

    modified the coding styles

---
+++ experience_details/selectionex.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+import MySQLdb
+
+db = MySQLdb.connect(host = "localhost", # your host, usually localhost
+                     user = "root", # your username
+                      passwd = "havas1993z", # your password
+                      db = "Employees") # name of the data base
+# you must create a Cursor object. It will let
+#  you execute all the queries you need
+cur = db.cursor() 
+
+# Use all the SQL you like
+cur.execute("select * from experience_details") 
+
+# print all the first cell of all the rows
+for row in cur.fetchall() :
+    print row
+cur.close()
