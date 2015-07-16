b8758689b8291b8a170d4ba87d183ce4b35f2b73
havasmohammed <havas@qburst.com>
2015-07-16 17:12:46 +0530

    modified coding styles

--- designation_details/join.py
+++ designation_details/join.py
@@ -3,14 +3,16 @@ import MySQLdb
 
 db = MySQLdb.connect(host="localhost", # your host, usually localhost
                      user="root", # your username
-                      passwd="havas1993z", # your password
-                      db="Employees") # name of the data base
+                     passwd="havas1993z", # your password
+                     db="Employees") # name of the data base
 # you must create a Cursor object. It will let
 #  you execute all the queries you need
 cur = db.cursor() 
 
 # Use all the SQL you like
-cur.execute("select employees.empno, employees.name, employees.designationid, employees.salary, designation_details.designation from employees left join designation_details on employees.designationid=designation_details.designationid")
+cur.execute("select employees.empno, employees.name, employees.designationid, employees.salary, \
+	designation_details.designation from employees left join designation_details \
+	on employees.designationid=designation_details.designationid")
 
 # print all the first cell of all the rows
 for row in cur.fetchall() :
