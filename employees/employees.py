b80622a562011354b66e7dc563aca4ba35b1a858
havasmohammed <havas@qburst.com>
2015-07-16 17:18:51 +0530

    modified by adding try catch,choices for designationid,followed the coding styles

--- employees/employees.py
+++ employees/employees.py
@@ -1,15 +1,25 @@
-import MySQLdb   
-db = MySQLdb.connect(host="localhost", user="root",passwd="havas1993z",db="Employees")
-x='y'
-while x=='y':
-	empno=int(raw_input( "Enter empno: "))
-	name=raw_input( "Enter name: ")
-	age=int(raw_input("Enter age: "))
-	designationid=int(raw_input("Enter designationid choices 1.441 2.442 3.443 4.444 5.445 6.446 "))
-	salary=int(raw_input("Enter salary: "))
-	#db= pyodbc.connect('DRIVER={SQL Server};SERVER=localhoast;DATABASE=tutorial')
+import MySQLdb  
+#name of the host, user, password and database 
+db = MySQLdb.connect(host="localhost", user="root", passwd="havas1993z", db="Employees")
+X='y'
+try:
+	#cursor object creation
 	cursor = db.cursor()
-	cursor.execute("Insert into employees Values (%s,%s,%s,%s,%s)",( name, age, designationid, salary, empno))
-	x=raw_input("do u want to continue(y/n)")
-db.commit()
+	while X == 'y':
+		empno = int(raw_input( "Enter empno: "))
+		name = raw_input( "Enter name: ")
+		age = int(raw_input("Enter age: "))
+		print "enter the designationid from the list"
+		cursor.execute("select * from designation_details")
+		for row in cursor.fetchall():
+			print "designationid: %d,designation: %s"%(row[0],row[1])
+		designationid = int(raw_input())
+		salary = int(raw_input("Enter salary: "))
+		#db= pyodbc.connect('DRIVER={SQL Server};SERVER=localhoast;DATABASE=tutorial')
+		cursor.execute("Insert into employees Values (%s, %s, %s, %s, %s)",\
+			( name, age, designationid, salary, empno))
+		X = raw_input("do u want to continue(y/n)")
+except Exception as e:
+	print str(e)
+	db.commit()
 cursor.close()
