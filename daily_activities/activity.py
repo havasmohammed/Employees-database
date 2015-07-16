f6e78e6466f48a806e33f2df961e147959d5e9f9
havasmohammed <havas@qburst.com>
2015-07-16 16:59:28 +0530

    modified by adding try catch

--- daily_activities/activity.py
+++ daily_activities/activity.py
@@ -1,16 +1,22 @@
 import MySQLdb   
 db = MySQLdb.connect(host="localhost", user="root",passwd="havas1993z",db="Employees")
-x='y'
-while x=='y':
-	empno=int(raw_input( "Enter empno: "))
-	date=raw_input( "Enter date: ")
-	project=raw_input("Enter project: ")
-	type=raw_input("Enter activitytype: ")
-	description=raw_input("Enter activity description: ")
-	timespent=int(raw_input( "Enter time spent: "))
-	#db= pyodbc.connect('DRIVER={SQL Server};SERVER=localhoast;DATABASE=tutorial')
+X = 'y'
+try:
+	#creation of cursor object
 	cursor = db.cursor()
-	cursor.execute("Insert into daily_activities Values (%s,%s,%s,%s,%s,%s)",(empno, date, project, type, description, timespent))
-	x=raw_input("do u want to continue(y/n)")
-db.commit()
+	while X == 'y':
+		empno = int(raw_input( "Enter empno: "))
+		date = raw_input( "Enter date: ")
+		project = raw_input("Enter project: ")
+		type = raw_input("Enter activitytype: ")
+		description = raw_input("Enter activity description: ")
+		timespent = int(raw_input( "Enter time spent: "))
+		#db= pyodbc.connect('DRIVER={SQL Server};SERVER=localhoast;DATABASE=tutorial')
+		#insert into tables
+		cursor.execute("Insert into daily_activities Values (%s,%s,%s,%s,%s,%s)",\
+			(empno, date, project, type, description, timespent))
+		X = raw_input("do u want to continue(y/n)")
+except Exception as e:
+	print str(e)
+	db.commit()
 cursor.close()
