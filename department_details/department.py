09d4ca7d77a1c1a3b601e4074c2d237b0a3c57ba
havasmohammed <havas@qburst.com>
2015-07-16 17:05:01 +0530

    modified coding styles

--- department_details/department.py
+++ department_details/department.py
@@ -1,12 +1,17 @@
 import MySQLdb   
-db = MySQLdb.connect(host="localhost", user="root",passwd="havas1993z",db="Employees")
-x='y'
-while x=='y':
-	empno=int(raw_input("Enter empno: "))
-	department=raw_input( "Enter department: ")
-	#db= pyodbc.connect('DRIVER={SQL Server};SERVER=localhoast;DATABASE=tutorial')
+db = MySQLdb.connect(host="localhost", user="root", passwd="havas1993z",db="Employees")
+X = 'y'
+try:
+	#creation of cursor object
 	cursor = db.cursor()
-	cursor.execute("Insert into department_details Values (%s,%s)",(empno, department))
-	x=raw_input("do u want to continue(y/n)")
-db.commit()
+	while X == 'y':
+		empno = int(raw_input("Enter empno: "))
+		department = raw_input( "Enter department: ")
+		#db= pyodbc.connect('DRIVER={SQL Server};SERVER=localhoast;DATABASE=tutorial')
+		cursor.execute("Insert into department_details Values (%s, %s)",\
+			(empno, department))
+		X = raw_input("do u want to continue(y/n)")
+except Exception as e:
+	print str(e)
+	db.commit()
 cursor.close()
